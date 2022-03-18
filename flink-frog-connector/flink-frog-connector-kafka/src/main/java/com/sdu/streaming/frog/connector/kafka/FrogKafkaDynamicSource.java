b5c4c1a3d5d564c93db3321a0c69e6d7ee33730a
hanhan.zhang <hanhan.zhang@192.168.1.13>
2022-03-18 22:32:51 +0800

    support kafka connector

---
+++ flink-frog-connector/flink-frog-connector-kafka/src/main/java/com/sdu/streaming/frog/connector/kafka/FrogKafkaDynamicSource.java
@@ -0,0 +1,53 @@
+package com.sdu.streaming.frog.connector.kafka;
+
+import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.streaming.connectors.kafka.config.StartupMode;
+import org.apache.flink.streaming.connectors.kafka.internals.KafkaTopicPartition;
+import org.apache.flink.streaming.connectors.kafka.table.KafkaDynamicSource;
+import org.apache.flink.table.connector.format.DecodingFormat;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.types.DataType;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+public class FrogKafkaDynamicSource extends KafkaDynamicSource {
+
+    public FrogKafkaDynamicSource(DataType physicalDataType,
+                                  DecodingFormat<DeserializationSchema<RowData>> keyDecodingFormat,
+                                  DecodingFormat<DeserializationSchema<RowData>> valueDecodingFormat,
+                                  int[] keyProjection,
+                                  int[] valueProjection,
+                                  String keyPrefix,
+                                  List<String> topics,
+                                  Pattern topicPattern,
+                                  Properties properties,
+                                  StartupMode startupMode,
+                                  Map<KafkaTopicPartition, Long> specificStartupOffsets,
+                                  long startupTimestampMillis,
+                                  boolean upsertMode,
+                                  String tableIdentifier) {
+        super(physicalDataType,
+                keyDecodingFormat,
+                valueDecodingFormat,
+                keyProjection,
+                valueProjection,
+                keyPrefix,
+                topics,
+                topicPattern,
+                properties,
+                startupMode,
+                specificStartupOffsets,
+                startupTimestampMillis,
+                upsertMode,
+                tableIdentifier);
+    }
+
+    @Override
+    public ScanRuntimeProvider getScanRuntimeProvider(ScanContext context) {
+        // TODO: supported source parallelism
+        return super.getScanRuntimeProvider(context);
+    }
+}
